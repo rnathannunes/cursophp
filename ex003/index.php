02bc0fecf4fbfecbc3a814c29b26beecdc15affe
Nathan Nunes <126528012+rnathannunes@users.noreply.github.com>
2023-06-26 16:46:13 -0300

    ex004

---
+++ ex003/index.php
@@ -0,0 +1,38 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+  </head>
+  <body>
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = octal
+        
+        //$num = 0x1A;
+        //echo "O valor da variável é $num + ";
+ 
+        //$v = "Nathan";
+        //var_dump($v);
+
+        //$num = 3e2; // 3 x 10(2)
+        //echo "O valor é $num";
+
+        //$num = (float) "950";
+        //var_dump($num);
+
+        //$casado = true;
+        //var_dump($casado);
+        //print "O valor para casado é $casado";
+
+        //$vet = [6, 2, 9, 3, 5];
+        //var_dump($vet)
+
+        //Class Pessoa {
+        //    private string $nome;
+        //}
+        //$p = new Pessoa;
+        //var_dump($p);
+    ?>
+  </body>
+  </html>
---
+++ ex004/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php
+            $nome = $_GET["nome"] ?? "sem nome";
+            $snome = $_GET["snome"] ?? "desconhecido";
+            echo "<p>É um prazer te conhecer, <strong>$nome $snome</strong>!<br>Este é o meu site!</p>";
+        ?>
+        <p>
+            <a href="javascript:history.go(-1)">Voltar para página anterior</a>
+        </p>
+    </main>
+</body>
+</html>
